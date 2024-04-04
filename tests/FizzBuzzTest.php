1799dceea97e5c43315b52684d586c980bc196c8
Bryan Useche <bryan.useche.10@gmail.com>
2024-04-03 22:42:36 -0400

    leccion 3

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -3,43 +3,21 @@
 namespace App\Tests;
 
 use App\FizzBuzz;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    public function testCuandoLeDoyElNumeroTresMeDevuelveFizz(): void
+    #[TestWith([1, 1])]
+    #[TestWith([3, "Fizz"])]
+    #[TestWith([5, "Buzz"])]
+    #[TestWith([15, "FizzBuzz"])]
+    public function testFizzBuzz($numeroATestear, $resultadoEsperado)
     {
         $fizzBuzz = new FizzBuzz();
 
-        $resultado = $fizzBuzz->diNumero(3);
+        $resultado = $fizzBuzz->diNumero($numeroATestear);
 
-        $this->assertEquals("Fizz", $resultado);
-    }
-
-    public function testCuandoLeDoyElNumeroCincoMeDevuelveBuzz(): void
-    {
-        $fizzBuzz = new FizzBuzz();
-
-        $resultado = $fizzBuzz->diNumero(5);
-
-        $this->assertEquals("Buzz", $resultado);
-    }
-
-    public function testCuandoLeDoyElNumeroQuinceMeDevuelveFizzBuzz(): void
-    {
-        $fizzBuzz = new FizzBuzz();
-
-        $resultado = $fizzBuzz->diNumero(15);
-
-        $this->assertEquals("FizzBuzz", $resultado);
-    }
-
-    public function testCuandoLeDoyElNumeroUnoMeDevuelveUno(): void
-    {
-        $fizzBuzz = new FizzBuzz();
-
-        $resultado = $fizzBuzz->diNumero(1);
-
-        $this->assertEquals(1, $resultado);
+        $this->assertEquals($resultadoEsperado, $resultado);
     }
 }
